2e630197bf918e80e13c2f746d56b0db1824ae05
Cybrosif <anej.dolercrep@gmail.com>
2023-09-18 19:11:11 +0200

    test

---
+++ Server/Server/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Protocol;
+using System.Text;
+using MQTT;
+
+
+namespace ServerApp
+{
+	public class Program
+	{
+	
+		static async Task Main(string[] args)
+		{
+			MQTT.mqtt mqtt = new MQTT.mqtt();
+			await mqtt.Handle_Received_Application_Message();
+		}
+	}
+}
+
---
+++ Server/Server/SystemInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemInfo
+{
+	public class SystemInfoData
+	{
+		public double CpuUsage { get; set; }
+		public ulong TotalMemoryMB { get; set; }
+		public ulong FreeMemoryMB { get; set; }
+		public ulong UsedMemoryMB { get; set; }
+		public string FileSystem { get; set; }
+		public long TotalSizeMB { get; set; }
+		public long AvailableFreeSpaceMB { get; set; }
+		public TimeSpan Uptime { get; set; }
+	}
+}
